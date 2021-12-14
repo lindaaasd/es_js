16393184ed6e728a9e2195c3c294f6c885ff5724
Linda Giurca <lindaaa@Lindas-MacBook-Air.local>
2021-12-14 16:45:32 +0100

    ES_5 done

---
+++ ES_5.js
@@ -0,0 +1,67 @@
+let giorniSettimana = prompt("inserisci numero del giorno")
+
+switch (giorniSettimana) {
+    case "1":
+        console.log ("lunedi")
+        break;
+    case "2":
+        console.log ("martedi")
+        break;
+    case "3":
+        console.log ("mercoledi")
+        break;
+    case "4":
+        console.log ("giovedi")
+        break;
+    case "5":
+        console.log ("venerdi")
+        break;
+    case "6":
+        console.log ("mercoledi")
+        break;
+    case "7":
+        console.log ("mercoledi")
+        break;        
+        
+    default: console.log ("Errore! Giorno della settimana non valido!")
+}
+
+
+
+
+    // if (i=1) {
+    //     console.log ("lunedi")
+    //     break;
+    // }
+    // if (i=2) {
+    //     console.log ("martedi")
+    //     break;
+    // }
+    // if (i=3) {
+    //     console.log ("mercoledi")
+    //     break;
+    // }
+
+    // if (i=4) {
+    //     console.log ("giovedi")
+    //     break;
+    // }
+    // if (i=5) {
+    //     console.log ("venerdi")
+    //     break;
+    // }
+
+    // if (i=6) {
+    //     console.log ("sabato")
+    //     break;
+    // }
+
+    // if (i=7) {
+    //     console.log ("domenica")
+    //     break;
+    // }
+
+    // else {
+    //     console.log ("Errore! Giorno della settimana non valido!")
+    // }
+

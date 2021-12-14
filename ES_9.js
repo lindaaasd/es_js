d3b366ad2751d5cc3302432e3e1641e7a70394d5
Linda Giurca <lindaaa@Lindas-MacBook-Air.local>
2021-12-14 23:01:43 +0100

    finito ES_9

---
+++ ES_9.js
@@ -0,0 +1,15 @@
+
+let MyString = " "
+
+
+for (let i= 1; i <= 100; i++) {
+
+MyString = " " + MyString + i + " "
+
+if ( i % 10 == 0) { 
+MyString = MyString + "\n"
+}
+
+}
+
+console.log (MyString)

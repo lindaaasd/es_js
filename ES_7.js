37cc2a7a97ee2825a1bd2281914a1bc7ff6a5386
Linda Giurca <lindaaa@Lindas-MacBook-Air.local>
2021-12-14 22:34:25 +0100

    migliorato codice ES_7

--- ES_7.js
+++ ES_7.js
@@ -5,16 +5,13 @@ let T2 = 0;
 
 for ( let i= 0; i < mythrow; i++ ) {
     dice = Math.floor(Math.random() * 6 + 1);
+    dice2 = Math.floor(Math.random() * 6 + 1);
     T1 = T1 + dice;
-    console.log ("Il primo tipo ha lanciato il dado " + dice + " per il totale di " + T1)
+    T2 = T2 + dice2;
+    console.log ("Il primo tipo ha lanciato il dado " + dice + " per il totale di " + T1);
+    console.log ("Il secondo tipo ha lanciato il dado " + dice2 + " per il totale di " + T2)
 }
 
-
-for ( let i= 0; i < mythrow; i++ ) {
-    dice = Math.floor(Math.random() * 6 + 1);
-    T2 = T2 + dice;
-    console.log ("Il secondo tipo ha lanciato il dado " + dice + " per il totale di " + T2)
-}
 
 if (T1 > T2) {
     console.log ("Ha vinto il primo tipo")

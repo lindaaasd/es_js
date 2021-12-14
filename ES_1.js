0430399a6135ef13f95272e1bf380bb28589d6cc
Linda Giurca <lindaaa@Lindas-MacBook-Air.local>
2021-12-14 15:52:39 +0100

    correzioni piccole

--- ES_1.js
+++ ES_1.js
@@ -6,4 +6,7 @@ let e = 15;
 let f = -6;
 let g = 7;
 
-console.log (Math.max(a, b, c, d, e, f, g))
+let x = Math.max(a, b, c, d, e, f, g);
+let y = Math.min(a, b, c, d, e, f, g);
+console.log ("La temperatura piu' calda e' " + x + " e quella piu’ fredda " + y)
+

044f74f1325fa3e827ff6dedc1e6b2e49bebaf5a
Linda Giurca <lindaaa@Lindas-MacBook-Air.local>
2021-12-15 15:57:15 +0100

    finito ES_11

---
+++ ES_11.js
@@ -0,0 +1,16 @@
+let myFirstArray = [1, 2, 3, 4, 5];
+
+function minArray (tt){
+    let mini = Math.min(...tt)
+
+    result = console.log ("Il valore minore della fantastica lista è " + mini)
+    }
+
+function maxArray (pp){
+let maxi = Math.max (...pp)
+
+result = console.log ("Il valore più grande della fantastica lista è " + maxi)
+}
+
+minArray (myFirstArray)
+maxArray (myFirstArray)
